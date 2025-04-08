3fbb2bd3867b21ddbe08fec4a30bb0495879e010
Erne1357 <villarreal.i.ernesto@gmail.com>
2025-04-08 03:43:20 -0600

    Modificación de la insercción de roles, completando los roles totales de la aplicación hasta el momento

--- database/DML/insert_role.sql
+++ database/DML/insert_role.sql
@@ -1,3 +1,7 @@
 INSERT INTO role (name, description)
-VALUES ('applicant', 'Usuario aplicante')
+VALUES ('applicant', 'Usuario aplicante del sistema'),
+        ('social_service', 'Usuario de servicio social con la opcion unica de revisar documentos'),
+        ('program_admin', 'Usuario administrador del programa con la opcion de ver estadisticas y todo lo relacionado con su programa'),
+        ('posgraduate_admin', 'Usuario administrador con el mayor número de privilegiso, puede crear reportes y estadisticas de todos los programas del postgrado')
 RETURNING id;
+
