af3a07af05063153f2bb8fbf5dbedea1eb151c37
Erne1357 <villarreal.i.ernesto@gmail.com>
2025-05-15 05:26:53 -0600

    Modificación de información especifica de los programas

--- app.py
+++ app.py
@@ -4,23 +4,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
-from flask import Flask, render_template
-from app import Config
-
-def create_app():
-    # Create the Flask application
-    app = Flask(__name__,template_folder='app/templates', static_folder='app/static')
-    app.config.from_object(Config)
-    
-    # Define a basic route for login
-    @app.route('/login')
-    def login():
-        return render_template('auth/login.html')
-    
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    # Run the app on host 0.0.0.0 so it's accessible from Docker
-    app.run(host='0.0.0.0', debug=True)
